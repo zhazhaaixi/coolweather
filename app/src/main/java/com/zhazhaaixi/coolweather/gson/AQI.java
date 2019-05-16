ed380dbc223278dab88c7adced07973b379bc7a7
zhazhaaixi <zhazhaaixi1@163.com>
2019-05-16 10:22:09 +0800

    加入显示天气信息

---
+++ app/src/main/java/com/zhazhaaixi/coolweather/gson/AQI.java
@@ -0,0 +1,10 @@
+package com.zhazhaaixi.coolweather.gson;
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+}
--- app/src/main/java/com/zhazhaaixi/coolweather/util/Utility.java
+++ app/src/main/java/com/zhazhaaixi/coolweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.zhazhaaixi.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.zhazhaaixi.coolweather.db.City;
 import com.zhazhaaixi.coolweather.db.County;
 import com.zhazhaaixi.coolweather.db.Province;
+import com.zhazhaaixi.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -77,5 +79,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
